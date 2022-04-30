7449d5a2686fdc6818e579ce15ca7f8b5b5e7826
Nozhan Amiri <nozhana@gmail.com>
2022-04-30 17:39:30 +0430

    Initial Commit

---
+++ stocks-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stocks-clone
+//
+//  Created by Nozhan Amiri on 4/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
