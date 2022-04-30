f593d6e71fc920f1f6ab27a54833b13c77928e85
Nozhan Amiri <nozhana@gmail.com>
2022-04-30 18:00:22 +0430

    Programmatic UI Setup

---
+++ stocks-clone/Controllers/TopStoriesNewsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TopStoriesNewsViewController.swift
+//  stocks-clone
+//
+//  Created by Nozhan Amiri on 4/30/22.
+//
+
+import UIKit
+
+class TopStoriesNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
