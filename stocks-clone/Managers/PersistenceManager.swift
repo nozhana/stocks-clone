fdd7cbef7a7ce9f61ebbe8b59ab0e3db0f5efb6c
Nozhan Amiri <nozhana@gmail.com>
2022-05-02 01:46:05 +0430

    Stubs and skeleton for PersistenceManager

--- stocks-clone/Managers/PersistenceManager.swift
+++ stocks-clone/Managers/PersistenceManager.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+final class PersistenceManager {
+    /// Singleton of parent class
+    static let shared = PersistenceManager()
+    
+    /// Shared instance of `UserDefaults`
+    private let userDefaults: UserDefaults = .standard
+    
+    /// Struct to hold constants across `PersistenceManager`
+    private struct Constants {
+        
+    }
+    
+    private init() {}
+    
+//    MARK: - Public
+    
+    public var watchlist: [String] { [] }
+    
+    public func addToWatchlist () {
+        
+    }
+    
+    public func removeFromWatchlist () {
+        
+    }
+    
+//    MARK: - Private
+    
+    private var hasOnboarded: Bool { false }
+    
+    
+    
+}
