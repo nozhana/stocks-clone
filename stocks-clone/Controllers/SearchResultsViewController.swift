e999ea447be4f4cacd5d9cbfb1717982328b4caa
Nozhan Amiri <nozhana@gmail.com>
2022-05-03 20:23:11 +0430

    Setup TableView in SearchResultsViewController
    SearchResultsViewController is a delegate and data source of encompassed TableView

--- stocks-clone/Controllers/SearchResultsViewController.swift
+++ stocks-clone/Controllers/SearchResultsViewController.swift
@@ -8,22 +8,59 @@
 import UIKit
 
 class SearchResultsViewController: UIViewController {
+    
+//    MARK: - Properties
 
+    private let tableView: UITableView = {
+        let table = UITableView()
+//        Register a cell
+        table.register(SearchResultsTableViewCell.self,
+                       forCellReuseIdentifier: SearchResultsTableViewCell.identifier)
+        return table
+    }()
+    
+//    MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setupTable()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
-    */
+    
+//    MARK: - Private
+    
+    private func setupTable() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
 
+extension SearchResultsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        TODO: Pass real numberOfRows from API
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: SearchResultsTableViewCell.identifier,
+            for: indexPath
+        )
+        
+//        TODO: Pass real stocks from API
+        cell.textLabel?.text = "AAPL"
+        cell.detailTextLabel?.text = "Apple Inc."
+        
+        return cell
+        
+    }
+    
+    
 }
