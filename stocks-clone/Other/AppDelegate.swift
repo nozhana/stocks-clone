637e7a5df909709111d64f2cb42d774b34240381
Nozhan Amiri <nozhana@gmail.com>
2022-05-05 20:23:53 +0430

    Stub debug()

--- stocks-clone/Other/AppDelegate.swift
+++ stocks-clone/Other/AppDelegate.swift
@@ -15,14 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        APIManager.shared.search(query: "AAPL") { result in
-            switch result {
-            case .success(let response):
-                print(response)
-            case .failure(let error):
-                print(error)
-            }
-        }
+       debug()
         
         return true
     }
@@ -41,6 +34,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    
+    private func debug() {
+        
+    }
 
 }
-
