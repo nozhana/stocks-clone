065e8e3dc52273237a21fd994c26f554774655cf
Nozhan Amiri <nozhana@gmail.com>
2022-05-09 11:03:32 +0430

    Add frame size shorthands
    Add Extensions.swift
    Create UIView extension

---
+++ stocks-clone/Other/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  stocks-clone
+//
+//  Created by Nozhan Amiri on 5/8/22.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Framing
+
+extension UIView {
+    var w: CGFloat {
+        frame.width
+    }
+    
+    var h: CGFloat {
+        frame.height
+    }
+    
+    var l: CGFloat {
+        frame.minX
+    }
+    
+    var r: CGFloat {
+        frame.maxX
+    }
+    
+    var t: CGFloat {
+        frame.minY
+    }
+    
+    var b: CGFloat {
+        frame.maxY
+    }
+}
