7af4751bf7181c7413edab132f595a1b5618a2a4
Nozhan Amiri <nozhana@gmail.com>
2022-05-23 01:28:57 +0430

    Stub StockChartView

---
+++ stocks-clone/Views/StockChartView.swift
@@ -0,0 +1,29 @@
+//
+//  StockChartView.swift
+//  stocks-clone
+//
+//  Created by Nozhan Amiri on 5/19/22.
+//
+
+import UIKit
+
+class StockChartView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    /// Reset the chart view
+    func reset() {
+        
+    }
+
+}
