64230e8079772aac681be9fd83a54447d14d3e71
Nozhan Amiri <nozhana@gmail.com>
2022-05-28 19:07:40 +0430

    Update StockDetailsVC
    Create tableView and register as NewsStory
    Stub private funcs

--- stocks-clone/Controllers/StockDetailsViewController.swift
+++ stocks-clone/Controllers/StockDetailsViewController.swift
@@ -15,6 +15,15 @@ class StockDetailsViewController: UIViewController {
     private let companyName: String
     private var candleSticks: [CandleStick]
     
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(NewsHeaderView.self,
+                           forHeaderFooterViewReuseIdentifier: NewsHeaderView.identifier)
+        tableView.register(NewsStoryTableViewCell.self,
+                           forCellReuseIdentifier: NewsStoryTableViewCell.identifier)
+        return tableView
+    }()
+    
 //    MARK: - Init
     
     init(
@@ -37,10 +46,28 @@ class StockDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-//        TODO: Show View
-//        TODO: Financial Data
-//        TODO: Show Chart
-//        TODO: News
+        
+        setupTable()
+        fetchData()
+        fetchNews()
     }
 
+//    MARK: - Private
+    
+    private func setupTable() {
+//        TODO: Show view
+    }
+    
+    private func fetchData() {
+//        TODO: Fetch financial data
+        renderChart()
+    }
+    
+    private func renderChart() {
+//        TODO: Show chart
+    }
+    
+    private func fetchNews() {
+//        TODO: Fetch news
+    }
 }
