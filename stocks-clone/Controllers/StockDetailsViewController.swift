74dbf1e72b686f5a289798933ccbbda5bb257578
Nozhan Amiri <nozhana@gmail.com>
2022-05-28 19:35:43 +0430

    Show company news in StockDetailsVC
    Implement fetchNews()
    #FIXUP Rename fetchData() -> fetchFinancialData

--- stocks-clone/Controllers/StockDetailsViewController.swift
+++ stocks-clone/Controllers/StockDetailsViewController.swift
@@ -24,6 +24,8 @@ class StockDetailsViewController: UIViewController {
         return tableView
     }()
     
+    private var stories: [NewsStory] = []
+    
 //    MARK: - Init
     
     init(
@@ -48,17 +50,25 @@ class StockDetailsViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         setupTable()
-        fetchData()
+        fetchFinancialData()
         fetchNews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 
 //    MARK: - Private
     
     private func setupTable() {
 //        TODO: Show view
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-    private func fetchData() {
+    private func fetchFinancialData() {
 //        TODO: Fetch financial data
         renderChart()
     }
@@ -69,5 +79,66 @@ class StockDetailsViewController: UIViewController {
     
     private func fetchNews() {
 //        TODO: Fetch news
+        APIManager.shared.news(for: .companyNews(symbol: symbol)) { [weak self] result in
+            switch result {
+            case .success(let stories):
+                DispatchQueue.main.async {
+                    self?.stories = stories
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                print("News Error: \(error)")
+            }
+        }
+    }
+}
+
+// MARK: - Protocol conformations
+
+// MARK: - UITableView Delegate/DataSource
+extension StockDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        stories.count
+    }
+    
+//    MARK: Cells
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: NewsStoryTableViewCell.identifier,
+            for: indexPath) as? NewsStoryTableViewCell else {
+            fatalError("Cell couldn't be cast as NewsStoryTableViewCell")
+        }
+        cell.configure(with: .init(model: stories[indexPath.row]))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        NewsStoryTableViewCell.preferredHeight
+    }
+    
+//    MARK: Header
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: NewsHeaderView.identifier) as? NewsHeaderView else {
+            fatalError("Header couldn't be cast as NewsHeaderView")
+        }
+        header.delegate = self
+        header.configure(with: .init(
+            title: symbol.uppercased(),
+//            TODO: Show add button if symbol not in watchlist
+            shouldShowAddButton: true
+        ))
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        NewsHeaderView.preferredHeight
+    }
+}
+
+// MARK: - NewsHeaderViewDelegate
+extension StockDetailsViewController: NewsHeaderViewDelegate {
+    func newsHeaderViewDidTapAddButton(_ headerView: NewsHeaderView) {
+//        TODO: Add to watchlist
     }
 }
